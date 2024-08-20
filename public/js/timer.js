19d931993c9ef85bc03f5e7d6882eacffb2855c8
Abenezer <abenagas59@gmail.com>
2024-08-20 23:54:39 +0300

    added demo timer

---
+++ public/js/timer.js
@@ -0,0 +1,120 @@
+function getSecondsUntilNextIntervalDog() {
+  const now = new Date();
+  const intervalStart = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    0,
+    1,
+    0,
+    0
+  );
+  const msPerInterval = 10 * 60 * 1000; // 10 minutes
+  return (
+    Math.floor(
+      msPerInterval -
+        ((now.getTime() - intervalStart.getTime()) % msPerInterval)
+    ) / 1000
+  );
+}
+
+function updateTimerDog(dogTimerCounter) {
+  const secondsLeft = getSecondsUntilNextIntervalDog();
+  const minutes = Math.floor(secondsLeft / 60);
+  const seconds = parseInt(secondsLeft % 60);
+  //   console.log(`${minutes}:${seconds.toString().padStart(2, "0")}`);
+  if (dogTimerCounter) {
+    dogTimerCounter.textContent = `${minutes}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  } else {
+    console.warn("Timer element not found");
+  }
+}
+
+function startTimerDog() {
+  const timerCheckInterval = setInterval(() => {
+    const dogTimerCounter = document.getElementById("dog-timer-label");
+    if (dogTimerCounter) {
+      clearInterval(timerCheckInterval); // Stop checking once the element is found
+
+      function update() {
+        updateTimerDog(dogTimerCounter);
+        requestAnimationFrame(update);
+      }
+      update();
+    }
+  }, 100);
+}
+
+// Horse
+
+function getSecondsUntilNextIntervalHorse() {
+  const now = new Date();
+  const intervalStart = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    0,
+    6,
+    0,
+    0
+  );
+  const msPerInterval = 10 * 60 * 1000; // 10 minutes
+  return (
+    Math.floor(
+      msPerInterval -
+        ((now.getTime() - intervalStart.getTime()) % msPerInterval)
+    ) / 1000
+  );
+}
+
+function updateLabelHorse() {
+  const now = new Date();
+  const start = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    0,
+    5,
+    59,
+    0
+  );
+  const msPerInterval = 10 * 60 * 1000; // 10 minutes
+  const initialValue = 986;
+  const intervalsPassed = Math.floor(
+    (now.getTime() - start.getTime()) / msPerInterval
+  );
+  horseLabelmm.textContent = initialValue + intervalsPassed;
+}
+
+function updateTimerHorse(horseTimerCounter) {
+  const secondsLeft = getSecondsUntilNextIntervalHorse();
+  const minutes = Math.floor(secondsLeft / 60);
+  const seconds = parseInt(secondsLeft % 60);
+
+  if (horseTimerCounter) {
+    horseTimerCounter.textContent = `${minutes}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  } else {
+    console.warn("Timer element not found");
+  }
+}
+
+function startTimerHorse() {
+  const timerCheckInterval = setInterval(() => {
+    const horseTimerCounter = document.getElementById("horse-timer-label");
+    if (horseTimerCounter) {
+      clearInterval(timerCheckInterval); // Stop checking once the element is found
+
+      function update() {
+        updateTimerHorse(horseTimerCounter);
+        requestAnimationFrame(update);
+      }
+      update();
+    }
+  }, 100);
+}
+
+// startTimerHorse();
